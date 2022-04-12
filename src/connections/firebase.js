537d4474784614b7c953f653833a524fd755951c
joaovitorribeiroc1@gmail.com <joaovitorribeiroc1@gmail.com>
2022-04-12 17:35:45 -0300

    firebase que tinha apagado no merge foi adicionado novamente

---
+++ src/connections/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from '@firebase/app';
+import { getFirestore } from '@firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAGTvA79pDUGHVQgNCoLxBEkyi-JLt1Uws',
+    authDomain: 'html-css-js-98aaf.firebaseapp.com',
+    projectId: 'html-css-js-98aaf',
+    storageBucket: 'html-css-js-98aaf.appspot.com',
+    messagingSenderId: '294499340289',
+    appId: '1:294499340289:web:b6f09bfd4ba39d21f57000',
+};
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
